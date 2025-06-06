82a42a0dac7b45a600a055e5a2b1dfcd065ee1c8
SautiereQDev <contact@quentinsautiere.com>
2025-06-06 21:18:20 +0200

    feat: add ContactDto for validating contact form submissions

---
+++ src/contact/dto/contact.dto.ts
@@ -0,0 +1,26 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  MaxLength,
+} from '@nestjs/class-validator';
+
+export class ContactDto {
+  @IsNotEmpty({ message: 'Le nom est obligatoire' })
+  @MaxLength(100, { message: 'Le nom ne peut excéder 100 caractères' })
+  nom: string;
+
+  @IsOptional()
+  @MaxLength(100, {
+    message: "Le nom de l'entreprise ne peut excéder 100 caractères",
+  })
+  entreprise: string;
+
+  @IsEmail({}, { message: "L'adresse e-mail n'est pas valide" })
+  @IsNotEmpty({ message: "L'adresse e-mail est obligatoire" })
+  email: string;
+
+  @IsNotEmpty({ message: 'Le message est obligatoire' })
+  @MaxLength(2000, { message: 'Le message ne peut excéder 2000 caractères' })
+  message: string;
+}
