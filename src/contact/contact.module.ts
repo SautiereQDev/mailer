e0c5c035f2d4da137dbc983dca2f00b9b0cfea09
SautiereQDev <contact@quentinsautiere.com>
2025-06-08 01:28:50 +0200

    fix import bug

--- src/contact/contact.module.ts
+++ src/contact/contact.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { ContactController } from './contact.controller';
 import { ContactService } from './contact.service';
+import { BlacklistModule } from '../blacklist/blacklist.module';
 
 @Module({
-  imports: [MailerModule],
+  imports: [MailerModule, BlacklistModule],
   controllers: [ContactController],
   providers: [ContactService],
 })
